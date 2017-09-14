26b0c573db16bd1246ca6c1b08ffde00c99e07ba
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2017-09-14 10:46:30 -0400

    Added two more interface methods and code for dukeItOut()

--- Duel Justin Yau/src/main/Duel.java
+++ Duel Justin Yau/src/main/Duel.java
@@ -21,7 +21,18 @@ public class Duel {
 	}
 	
 	public void dukeItOut(){
-		
+		System.out.print(d1.getName());
+		pause(600);
+		System.out.print(" vs ");
+		pause(600);
+		System.out.println(d2.getName());
+		pause(600);
+		d1.taunt();
+		pause(2000);
+		d2.taunt();
+		pause(2000);
+		d1.setStartingHP(dueler1HP);
+		d2.setStartingHP(dueler2HP);
 	}
 	
 	public class CharacterA implements Dueler {
@@ -32,6 +43,14 @@ public class Duel {
 		
 		public String getName() {
 			return "Hello";
+		}
+		
+		public void setStartingHP(int hp) {
+			
+		}
+		
+		public int getHP() {
+			return 0;
 		}
 		
 	}
@@ -46,6 +65,27 @@ public class Duel {
 			return "Hello";
 		}
 		
+		public void setStartingHP(int hp) {
+			
+		}
+		
+		public int getHP() {
+			return 0;
+		}
+		
+	}
+	
+	/**
+	 * This method involves "Threads" which we will get to much later in the year, essentially, it
+	 * creates a pause in the execution of the program, which makes it more dramatic
+	 * @param time
+	 */
+	public void pause(long time){
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
