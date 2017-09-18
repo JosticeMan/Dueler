ec5aae36c28e76b2c7cf7b3bddab638b22a00d48
NoGhost <NoGhost@DESKTOP-IJR5TA5.home>
2017-09-17 21:59:15 -0400

    I have no idea

--- Duel Justin Yau/src/main/CharacterB.java
+++ Duel Justin Yau/src/main/CharacterB.java
@@ -3,19 +3,28 @@ package main;
 public class CharacterB implements Dueler {
 
 	private int hp = -1;
+	private boolean isLoaded = false;
+	private String[] theTaunts = {"You will never get to shoot me!", "I am sad to say that you will lose!", "You'll never get my cookies!", "I'm too good for you.", "You should've never challenged me!"};
+	private int lastTaunt = -1;
 	
 	public CharacterB() {
 	
 	}
 	
 	public void taunt() {
+		int numOfTaunts = theTaunts.length;
+		int randomNum = (int) (Math.random()*numOfTaunts);
+		while(lastTaunt == randomNum) {
+			randomNum = (int) (Math.random()*numOfTaunts);
+		}
+		lastTaunt = randomNum;
 		
-		System.out.println("You will never shoot me!");
+		System.out.println(this.getName() + ": " + theTaunts[randomNum]);
 	}
 	
 	public String getName() {
 		
-		return "Justin";
+		return "Bot";
 	}
 	
 	public void setStartingHP(int hp) {
@@ -35,5 +44,36 @@ public class CharacterB implements Dueler {
 		return d.getHP() == hp;
 	}
 	
+	public int getAction(Object caller) {
+		if(caller instanceof CharacterA) {
+			return 3;
+		}
+		else {
+			int randomNum = (int) Math.random()*3;
+			while(randomNum == 0) {
+				if(isLoaded) {
+					randomNum = (int) Math.random()*3;
+				}
+				else {
+					if(randomNum == 1) {
+						isLoaded = false;
+						return randomNum;
+					}
+					else {
+						randomNum = 2;
+						return randomNum;
+					}
+				}
+			}
+			
+		}
+			
+	}
+	public void hit(Object caller) {
+		if(caller instanceof Duel) {
+			hp = hp - 10;
+		}
+	}
+	}
 	
 }
